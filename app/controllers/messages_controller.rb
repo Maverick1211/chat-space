426b0c0e87e8429ff8d695b86f7fced6577f23da
Maverick1211 <tensho1211@icloud.com>
2016-12-16 01:58:39 +0900

    edit few files
    except group, model, routes.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,24 @@
 class MessagesController < ApplicationController
-  def index
-    @user = current_user
-  end
+	def index
+	  @groups = current_user.groups
+	  @group = Group.find(params[:group_id])
+	  @users = @group.users
+	  @message = Message.new
+	  @messages = Message.where(group_id: params[:group_id])
+	end
+
+	def create
+	  @group = Group.find(params[:group_id])
+	  @message = Message.new(message_params)
+	  if @message.save
+	  	redirect_to group_messages_path(@group)
+	  else
+	  	redirect_to group_messages_path(@group), notice: "please create again"
+	  end
+	end
+
+	private
+	def message_params
+		params.require(:message).permit(:image, :body).merge(user_id: current_user.id, group_id: @group.id)
+	end
 end
