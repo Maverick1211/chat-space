a18671bbb0d6fccdc5d2c5390afa68c99e1726b5
Maverick1211 <tensho1211@icloud.com>
2016-12-23 14:56:58 +0900

    delete blank line and comment out

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -11,9 +11,7 @@ $(function() {
   $('.chat-footer').on('submit', function(e) {
     e.preventDefault();
     var textField = $('#message_body');
-    // console.log(textField);
     var message = textField.val();
-    // console.log(message);
     $.ajax({
       type: 'POST',
       url: './messages',
@@ -27,13 +25,10 @@ $(function() {
     .done(function(data) {
       var html = buildHTML(data);
       chat_messages = $('.chat-messages').append(html);
-      // console.log(chat_messages);
       textfield = textField.val('');
-      // console.log(textfield);
     })
     .fail(function() {
       alert('please create again');
     });
   });
 });
-
