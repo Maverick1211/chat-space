e764533ab4293630cacccb53c1f2a2154b8627de
Maverick1211 <tensho1211@icloud.com>
2016-12-29 19:43:05 +0900

    function of adding members

--- app/assets/javascripts/incremental_search.js
+++ app/assets/javascripts/incremental_search.js
@@ -4,14 +4,24 @@ $(window).on('load', function(){
     var html;
     $.each(users, function(index, user){
       var user_name = $('<p class="chat-group-user__name">').append(user.name);
-      var a_tag = $('<a href="", class = "user-search-add chat-group-user__btn chat-group-user__btn--add">').append("追加")
-      var list = $('<div class="chat-group-user clearfix">').append(user_name).append(a_tag)
+      var a_tag = $('<a href="", class = "user-search-add chat-group-user__btn chat-group-user__btn--add">').attr({'id': "chat-group-user-add-url-" + user.id}).attr({ 'data-user-id': user.id, 'data-user-name': user.name }).append("追加")
+      var list = $('<div class="chat-group-user clearfix">').attr({'id': "chat-group-user-add-" + user.id}).append(user_name).append(a_tag)
       var list = list.prop('outerHTML');
       lists += list
     });
     var html = $(lists).wrapAll('<div id="user-search-result">');
     return html;
   }
+
+  function buildAddHTML(user_id, user_name) {
+    var html;
+    var input = $('<input name="group[user_ids][]" type="hidden">').attr({'value': user_id})
+    var user_name_tag = $('<p class="chat-group-user__name">').append(user_name);
+    var a_tag = $('<a href="", class = "user-search-add chat-group-user__btn chat-group-user__btn--remove">').attr({'id': "chat-group-user-remove-url-" + user_id}).attr({ 'data-user-id': user_id}).append("削除")
+    var html = $('<div class="chat-group-user clearfix">').attr({'id': "chat-group-user-remove-" + user_id}).append(input).append(user_name_tag).append(a_tag)
+    return html;
+  }
+
   var preFunc = null,
   ajaxSearch = function(input) {
     $.ajax({
@@ -25,12 +35,24 @@ $(window).on('load', function(){
     .done(function(data) {
       html = buildHTML(data.users);
       $("#user-search-result").html(html);
-
-      })
+      $(document).on("click", ".chat-group-user__btn--add", function(e){
+        e.preventDefault();
+        $(this).parent().remove();
+        var user_id = $(this).data('user-id');
+        var user_name = $(this).data('user-name');
+        add_member_html = buildAddHTML(user_id, user_name);
+        $("#chat-group-users").append(add_member_html);
+      });
+      $(document).on("click", ".chat-group-user__btn--remove", function(e){
+        e.preventDefault();
+        $(this).parent().remove();
+      });
+    })
     .fail(function() {
       alert('please create again');
     });
   };
+
   $('.chat-group-form__search').on('keyup', function(){
     input = $.trim($('.search_form').val());
       clearTimeout(preFunc);
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,7 +29,7 @@ class GroupsController < ApplicationController
 	end
 
 	def search
-	  @users = User.where('name LIKE(?)',"%#{search_params[:keyword]}%").order('name ASC')
+	  @users = User.where('name LIKE(?)',"%#{search_params[:keyword]}%").where.not(id: current_user.id).order('name ASC')
 	  respond_to do |format|
         format.html { redirect_to new_group_path(@group) }
         format.json { render json: { users: @users } }
