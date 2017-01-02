08f8ca806035813534ee7801e4ccf66f8faa5f80
Maverick1211 <tensho1211@icloud.com>
2017-01-02 23:11:53 +0900

    introduction S3
    ・dotenv-rails

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,28 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region: ENV['S3_REGION'],
+      path_style: true,
+  }
+
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+
+  case Rails.env
+    when 'production'
+      config.storage :fog
+      config.fog_directory = "tensho-space"
+      config.asset_host = "https://s3-website-ap-northeast-1.amazonaws.com/tensho-space"
+    when 'development'
+      config.storage :fog
+      config.fog_directory = "tensho-space"
+      config.asset_host = "https://s3-website-ap-northeast-1.amazonaws.com/tensho-space"
+    when 'test'
+      config.storage :file
+      config.fog_directory = "tensho-space"
+      config.asset_host = "https://s3-website-ap-northeast-1.amazonaws.com/tensho-space"
+  end
+end
+
